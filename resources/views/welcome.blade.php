8ede0a196eadc4e6edd3dba7b325ef6e96efca8d
Skeypgreys <Skeypgreys@gmail.com>
2020-07-31 18:37:55 +0300

    Тест ПК

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Тест Ноута
+                    Тест ПК
                 </div>
 
                 <div class="links">
