7f86cfabd0877a5ad232570eb5423b61b37e9eef
Skeypgreys <Skeypgreys@gmail.com>
2020-07-31 18:24:43 +0300

    Start

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Новый проект
+                    Тест ПК
                 </div>
 
                 <div class="links">
