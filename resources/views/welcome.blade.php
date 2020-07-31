dd00a0d69f74bbf9e0b54fc0ba60b87ee8dc3840
RINO <Skeypgreys@bk.ru>
2020-07-31 18:36:20 +0300

    Тест ноута

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Тест ПК
+                    Тест Ноута
                 </div>
 
                 <div class="links">
